08ec87d4b69b5399902ae9d1fa0200ccc3c4ce87
zubairkhawar <zubairkhawer@gmail.com>
2023-11-16 03:39:56 +0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
