f342ca3818fde3ea9329557de94ecaf6dd8d06b6
zubairkhawar <zubairkhawer@gmail.com>
2023-11-17 08:31:26 +0500

    header init

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hero from './components/hero.js';
+import Header from './components/header.js';
 import Footer from './components/footer.js';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -7,10 +8,12 @@ function App() {
   return (
     <div className="w-full max-w-[1440px] mx-auto bg-white h-screen">
      <BrowserRouter>
+     <Header />
       <Hero />
       <Routes>
         <Route path='/' element={<h1>Home</h1>}/>
         <Route path='/products' element={<h1>Products</h1>}/>
+        <Route path='/cart' element={<h1>Cart</h1>}/>
         <Route path='/contact' element={<h1>Logout</h1>}/>
         <Route path='/profile' element={<h1>Profile</h1>}/>
       </Routes>
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useState,useEffect}from 'react';
+import { Link } from "react-router-dom";
+import {CgMenuRight, CgClose} from 'react-icons/cg';
 
 const Header = () => {
-  return <div>Header</div>;
+  const [bg, setBg] = useState(false);
+  const [mobileNav, setMobileNav] = useState(false)
+  return(
+    <header className={`${bg ? 'bg-black py-4 lg:py-6' : 'bg-none'} fixed left-0 w-full py-8 z-10 transition-all duration-200`}>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a href="#" alt='logo' className="text-white text-2xl font-semibold">Furniture Haven</a>
+          <div className='text-2xl text-white md:hidden lg:text-3xl cursor-pointer'>{mobileNav ? <CgClose /> : <CgMenuRight />}</div>
+        </div>
+        <nav className='hidden md:flex'>
+          <ul className='uppercase text-white hover:border-b transition-all'>
+            <li><Link to='/home'>Home</Link></li>
+            <li><Link to='/products'>Products</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+            <li><Link to='/logout'>Logout</Link></li>
+            <li><Link to='/profile'>Profile</Link></li>
+            <button><Link to='/SignUp'>Login</Link></button>
+          </ul>
+        </nav>
+        <div className={`${mobileNav ? 'left-0' : '-left-full'} md:hidden fixed bottom-0m w-full max-w-xs h-screen transition-all`}>
+          <div className='bg-white w-full h-full shadow-2xl'>
+
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 };
 
 export default Header;
